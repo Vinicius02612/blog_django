c66dc40989211ad559e34640451f4c70ab2757c5
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-03-14 22:27:27 -0300

    alterando porta django

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -13,4 +13,4 @@ echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8080
