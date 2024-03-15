ea47c9163544ff63b5e63f79758c05e8622ebf46
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-03-14 23:20:15 -0300

    comando em arquivos .sh separados

---
+++ scripts/collectstatic.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo 'Executando collectstatic.sh'
+python manage.py collectstatic --noinput
---
+++ scripts/runserver.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo 'Executando runserver.sh'
+python manage.py runserver 0.0.0.0:8000
